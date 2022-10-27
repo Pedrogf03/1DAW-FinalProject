b35f997ae5b091dc19eb341a872ebfbf9c1fd9f4
Pedro <9827@cifpceuta.es>
2022-10-27 13:54:49 +0200

    27 10 22

--- html/listaJuegos.php
+++ html/listaJuegos.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <?php
-  require_once '../includes/conexion.php';
-  require_once '../includes/helpers.php';
-  ?>
+    <?php
+    require_once '../includes/conexion.php';
+    require_once '../includes/helpers.php';
+    ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
